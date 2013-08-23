aaf600f7b6f1ccc62a1ce3add0c585921ff518ed
RafaelMalgor <rafaelmalgor@gmail.com>
2013-08-23 16:43:30 -0300

    merge

--- src/magicsuggest-1.3.1.js
+++ src/magicsuggest-1.3.1.js
@@ -6,7 +6,7 @@
  *
  * Author: Nicolas Bize
  * Date: Feb. 8th 2013
- * Version: 1.3.0
+ * Version: 1.3.1
  * Licence: MagicSuggest is licenced under MIT licence (http://www.opensource.org/licenses/mit-license.php)
  */
 (function($)
@@ -299,6 +299,13 @@
             resultAsString: false,
 
             /**
+             * @cfg {String} resultsField
+             * <p>Name of JSON object property that represents the list of suggested objets</p>
+             * Defaults to <code>results</code>
+             */
+            resultsField: 'results',
+
+            /**
              * @cfg {String} selectionCls
              * <p>A custom CSS class to add to a selected item</p>
              * Defaults to <code>''</code>.
@@ -475,7 +482,7 @@
          */
         this.clear = function(isSilent)
         {
-            this.removeFromSelection(_selection.slice(0)); // clone array to avoid concurrency issues
+            this.removeFromSelection(_selection.slice(0), isSilent); // clone array to avoid concurrency issues
         };
 
         /**
@@ -506,7 +513,6 @@
         this.empty = function(){
             this.input.removeClass(cfg.emptyTextCls);
             this.input.val('');
-            ms.input.attr('disabled', false);
         };
 
         /**
@@ -516,6 +522,7 @@
         {
             this.container.removeClass('ms-ctn-disabled');
             cfg.disabled = false;
+            ms.input.attr('disabled', false);
         };
 
         /**
@@ -831,7 +838,11 @@
                             url: data,
                             data: params,
                             success: function(asyncData){
+<<<<<<< HEAD:src/magicsuggest-1.3.0.js
                                 json = typeof(asyncData)==='string'?JSON.parse(asyncData):asyncData;
+=======
+                                json = typeof(asyncData) === 'string' ? JSON.parse(asyncData) : asyncData;
+>>>>>>> original/master:src/magicsuggest-1.3.1.js
                                 self._processSuggestions(json);
                                 $(ms).trigger('load', [ms, json]);
                             },
@@ -846,7 +857,11 @@
                         if(data.length > 0 && typeof(data[0]) === 'string') { // results from array of strings
                             _cbData = self._getEntriesFromStringArray(data);
                         } else { // regular json array or json object with results property
+<<<<<<< HEAD:src/magicsuggest-1.3.0.js
                             _cbData = data[cfg.resultField] || data;
+=======
+                            _cbData = data[cfg.resultsField] || data;
+>>>>>>> original/master:src/magicsuggest-1.3.1.js
                         }
                     }
                     self._displaySuggestions(self._sortAndTrim(_cbData));
@@ -985,7 +1000,7 @@
                     resultItemEl.mouseover($.proxy(handlers._onComboItemMouseOver, ref));
                     html += $('<div/>').append(resultItemEl).html();
                 });
-                ms.combobox.html(html);
+                ms.combobox.append(html);
                 _comboItemHeight = ms.combobox.find('.ms-res-item:first').outerHeight();
             },
 
@@ -1043,7 +1058,7 @@
                 if(cfg.selectionPosition === 'inner') {
                     ms.input.width(0);
                     inputOffset = ms.input.offset().left - ms.selectionContainer.offset().left;
-                    w = ms.container.width() - inputOffset - (cfg.hideTrigger === true ? 16 : 42);
+                    w = ms.container.width() - inputOffset - 42;
                     ms.input.width(w);
                     ms.container.height(ms.selectionContainer.height());
                 }
@@ -1477,4 +1492,6 @@
         }
         return obj;
     };
+
+//    $.fn.magicSuggest.defaults = {};
 })(jQuery);
