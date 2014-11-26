76875d43e8e1f97da723957ef23eded2ad006eac
lpascuali <luisg.pascuali@gmail.com>
2014-11-26 11:53:14 -0300

    validate row value in function onblur

--- magicsuggest.js
+++ magicsuggest.js
@@ -1227,7 +1227,10 @@
                 ms.container.removeClass('ms-ctn-focus');
                 ms.collapse();
                 _hasFocus = false;
-                if(ms.getRawValue() !== '' && cfg.allowFreeEntries === true){
+ 		var inputValid = $.trim(ms.input.val()).length > 0 &&
+                (!cfg.maxEntryLength || $.trim(ms.input.val()).length <= cfg.maxEntryLength);
+
+                if(inputValid === true && ms.getRawValue() !== '' && cfg.allowFreeEntries === true){
                     var obj = {};
                     obj[cfg.displayField] = obj[cfg.valueField] = ms.getRawValue().trim();
                     ms.addToSelection(obj);
