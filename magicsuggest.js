ff0129254602977c0fc3c90b6d0c056d71a4e29f
Nikita Rushmanov <Nikita240@users.noreply.github.com>
2017-07-11 17:21:56 -0700

    Prepend close button instead of appending it

--- magicsuggest.js
+++ magicsuggest.js
@@ -1045,7 +1045,7 @@
                             // small cross img
                             delItemEl = $('<span/>', {
                                 'class': 'ms-close-btn'
-                            }).data('json', value).appendTo(selectedItemEl);
+                            }).data('json', value).prependTo(selectedItemEl);
 
                             delItemEl.click($.proxy(handlers._onTagTriggerClick, ref));
                         }
